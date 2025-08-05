33d110f80b5f3fc1ea0fe6d50a66f9253ee19a58
Luiz Felipe <lfelipecx@gmail.com>
2025-08-05 19:37:03 -0300

    feat: add ui for sign in and sign up form components

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,26 @@
+// import { AppWindowIcon, CodeIcon } from "lucide-react"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SingInForm from "./components/sign-in-form";
+import SingUpForm from "./components/sign-up-form";
+
+const Authentication = () => {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="sign-in">
+        <TabsList>
+          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <SingInForm />
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <SingUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Authentication;

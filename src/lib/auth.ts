e8b4692d9a5047ff63f8022021faf7ae92826e7d
Luiz Felipe <lfelipecx@gmail.com>
2025-08-06 14:30:02 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
@@ -17,5 +23,8 @@ export const auth = betterAuth({
   },
   session: {
     modelName: "sessionTable"
+  },
+  verification: {
+    modelName: "verificationTable"
   }
 });

0eb33406befb73967dfc1cb8f60cb4a173668e5e
Luiz Felipe <lfelipecx@gmail.com>
2025-08-06 12:52:12 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -8,8 +9,10 @@ const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
-    }
-  })
+    },
+  });
+
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -17,27 +20,31 @@ const Home = async () => {
       <div className="space-y-6">
         <div className="px-5">
           <Image
-          src="/banner-01.png"
-          alt="Leve uma vida com estilo"
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-full"
-        />
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
 
-         <div className="px-5">
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
           <Image
-          src="/banner-02.png"
-          alt="seja autentico"
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-full"
-        />
-         </div>
+            src="/banner-02.png"
+            alt="seja autentico"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
       </div>
     </>
   );

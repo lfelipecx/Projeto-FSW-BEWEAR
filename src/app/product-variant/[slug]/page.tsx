1349c2728cea8ffd1019c806b2b8f5d105c8956e
Luiz Felipe <lfelipecx@gmail.com>
2025-08-06 17:27:19 -0300

    feat: add product page

---
+++ src/app/product-variant/[slug]/page.tsx
@@ -0,0 +1,100 @@
+import { eq } from "drizzle-orm";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import VariantSelector from "@/app/product-variant/[slug]/components/variant-selector";
+import Footer from "@/components/common/footer";
+import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+
+import ProductActions from "./components/product-actions";
+
+interface ProductVariantPageProps {
+  params: Promise<{ slug: string }>;
+}
+const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
+  const { slug } = await params;
+  const productVariant = await db.query.productVariantTable.findFirst({
+    where: eq(productVariantTable.slug, slug),
+    with: {
+      product: {
+        with: {
+          variants: true,
+        },
+      },
+    },
+  });
+  if (!productVariant) {
+    return notFound();
+  }
+  const likelyProducts = await db.query.productTable.findMany({
+    where: eq(productTable.categoryId, productVariant.product.categoryId),
+    with: {
+      variants: true,
+    },
+  });
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col space-y-6">
+        <Image
+          src={productVariant.imageUrl}
+          alt={productVariant.name}
+          sizes="100vw"
+          height={0}
+          width={0}
+          className="h-auto w-full rounded-3xl"
+        />
+
+        <div className="px-5">
+          <VariantSelector
+            selectedVariantSlug={productVariant.slug}
+            variants={productVariant.product.variants}
+          />
+        </div>
+
+        <div className="px-5">
+          {/*DESCRIÇÃO*/}
+          <h2 className="text-lg font-semibold">
+            {productVariant.product.name}
+          </h2>
+          <h3 className="text-muted-foreground text-sm">
+            {productVariant.name}
+          </h3>
+          <h3 className="text-lg font-semibold">
+            {formatCentsToBRL(productVariant.priceInCents)}
+          </h3>
+        </div>
+
+        
+          <ProductActions />
+        
+
+        <div className="flex flex-col gap-4 px-5">
+          <Button className="rounded-full" size="lg" variant="outline">
+            Adicionar à sacola
+          </Button>
+          <Button className="rounded-full" size="lg">
+            Comprar agora
+          </Button>
+        </div>
+
+        <div className="px-5">
+          <p className="text-shadow-amber-600">
+            {productVariant.product.description}
+          </p>
+        </div>
+
+        <ProductList title="Produtos similares" products={likelyProducts} />
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default ProductVariantPage;
